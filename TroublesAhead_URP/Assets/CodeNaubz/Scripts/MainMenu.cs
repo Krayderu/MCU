a91a14f4908de2b5efc799ed635f512e68188731
Lucysq <noe.aubert01@gmail.com>
2023-07-03 11:10:45 +0200

    Update main menu

---
+++ TroublesAhead_URP/Assets/CodeNaubz/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
