97f28f0a04f403145ac38ea686c8d24f96321b8f
Sébastien-Rieder <creeder200@gmail.com>
2023-07-22 04:10:42 +0200

    Fixed everything and rebake

--- TroublesAhead_URP/Assets/CodeNaubz/Scripts/LevierInteract.cs
+++ TroublesAhead_URP/Assets/CodeNaubz/Scripts/LevierInteract.cs
@@ -10,10 +10,20 @@ public class LevierInteract : StepInteractObject
     public Animator animator;
     [SerializeField] private FocusMode focusMode;
 
+    //MatChange
+    public Light redLight;
+    public Light greenLight;
+    public GameObject objToChange;
+    public Material newMaterial;
+    public Material[] mat;
+    Renderer rlRenderer;
+
     // Start is called before the first frame update
     public override void Start()
     {
         base.Start();
+        rlRenderer = objToChange.GetComponent<Renderer>();
+        mat = rlRenderer.materials;
     }
 
     public override void StepInteract()
@@ -32,6 +42,15 @@ public class LevierInteract : StepInteractObject
         ExitFocus();
 
         minimumStep = 50;
+    }
+
+    public void ChangeLight()
+    {
+
+        mat[0] = newMaterial;
+        rlRenderer.materials = mat;
+        redLight.enabled = false;
+        greenLight.enabled = true;
     }
 
 }
