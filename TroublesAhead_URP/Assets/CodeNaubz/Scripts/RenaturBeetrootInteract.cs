291bcceece54d27d9d936fd7cc4f04bfacc36ddd
Lucysq <noe.aubert01@gmail.com>
2023-07-23 13:07:51 +0200

    Started adding lore scripts

---
+++ TroublesAhead_URP/Assets/CodeNaubz/Scripts/RenaturBeetrootInteract.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class RenaturBeetrootInteract : InteractObject
+{
+
+    public Flowchart flowchart;
+    [SerializeField] private FocusMode focusMode;
+
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Interact()
+    {
+
+        base.Interact();
+
+        focusMode.EnableFocusMode();
+
+        flowchart.ExecuteBlock("RenaturBeetroot");
+
+    }
+
+}
